d9cba245dea34c9f8947a467409104866519f5ef
Ubuntu <henrique.magno@hotmail.com>
2023-03-02 23:55:16 +0000

    Exercicio 2

---
+++ Aula03/Exercicio02/copiarArquivo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE *arquivo;
+	FILE *saida;
+
+	char nomeArquivo[100], c;
+
+	saida = fopen("saida.txt" ,"a");
+
+	printf("Digite o nome do arquivo \n");
+	scanf("%s", nomeArquivo);
+
+	arquivo = fopen(nomeArquivo, "r");
+	if (arquivo == NULL)
+	{
+		printf("Cannot open file \n");
+		exit(0);
+	}
+
+	c = fgetc(arquivo);
+	while (c != EOF)
+	{
+		 fprintf(saida,"%c",c);
+		c = fgetc(arquivo);
+	}
+
+	fclose(arquivo);
+	return 0;
+}
