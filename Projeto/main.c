3d9651dcebd757ae3304c07c116123dd22c67613
henriquemgn <32944009+henriquemgn@users.noreply.github.com>
2023-03-16 19:43:36 -0300

    Update main.c

--- Projeto/main.c
+++ Projeto/main.c
@@ -13,6 +13,7 @@ irá comer, e após abaixar os garfos o proximo da lista comerá e o proximo dis
 *****/
 
 #include <stdio.h>
+#include <unistd.h> // sleep
 
 void imprimir(char *mesa[], int fome[]){
     for(int i = 0; i < 10; i++){
@@ -20,13 +21,25 @@ void imprimir(char *mesa[], int fome[]){
     }
 }
 
+char** resetGarfos(char *mesa[]){
+    mesa[0] = "1";
+    mesa[2] = "1";
+    mesa[4] = "1";
+    mesa[6] = "1";
+    mesa[8] = "1";
+    
+    return mesa;
+}
+
 char* verificadorFome(int fome[], char *mesa[]){
+    
     if(fome[0] >= fome[1] && fome[0] >= fome[2] && fome[0] >= fome[3] && fome[0] >= fome[4]){
         if (mesa[0] == "1" && mesa[2] == "1"){
             return "fome1";
         }else if(fome[1] >= fome[2] && fome[1] >= fome[3] && fome[1] >= fome[4] && mesa[2]=="1" && mesa[4]=="1"){
             return "fome2";
         }else if(fome[2] >= fome[3] && fome[2] >= fome[4] && mesa[4]=="1" && mesa[6]=="1"){
+            printf("teste1");
             return "fome3";
         }else if(fome[3] >= fome[4] && mesa[6]=="1" && mesa[8]=="1"){
             return "fome4";
@@ -39,6 +52,7 @@ char* verificadorFome(int fome[], char *mesa[]){
         if (mesa[2] == "1" && mesa[4] == "1"){
             return "fome2";
         }else if(fome[2] >= fome[3] && fome[2] >= fome[4] && mesa[4]=="1" && mesa[6]=="1"){
+            printf("teste2");
             return "fome3";
         }else if(fome[3] >= fome[4] && mesa[6]=="1" && mesa[8]=="1"){
             return "fome4";
@@ -49,6 +63,7 @@ char* verificadorFome(int fome[], char *mesa[]){
     
     if(fome[2] >= fome[3] && fome[2] >= fome[4]){
         if (mesa[4] == "1" && mesa[6] == "1"){
+            printf("teste3");
             return "fome3";
         }else if(fome[3] >= fome[4] && mesa[6]=="1" && mesa[8]=="1"){
             return "fome4";
@@ -60,10 +75,10 @@ char* verificadorFome(int fome[], char *mesa[]){
     if(fome[3] >= fome[4]){
         if (mesa[6] == "1" && mesa[8] == "1"){
             return "fome3";
-        }else if(mesa[8] == "1" && mesa[0] == "1"){
+        }
+    }else if(mesa[8] == "1" && mesa[0] == "1"){
             return "fome5";
         }
-    }
     
     else{
         return "fome5";
@@ -71,16 +86,63 @@ char* verificadorFome(int fome[], char *mesa[]){
 }
 
 void filosofosComem(int fome[], char *mesa[]){
-    if (fome[0] == 0 && fome[1] == 0 && fome[2] == 0 && fome[3] == 0 && fome[4] == 0){
+    
+    int pip[2];
+    pipe(pip);
+    
+    if (fome[0] <= 0 && fome[1] <= 0 && fome[2] <= 0 && fome[3] <= 0 && fome[4] <= 0){
         printf("\n Todos terminaram de comer");
     }
     
-    if (verificadorFome(fome,mesa) == "fome1"){
-        fome[0] = 80;
-        mesa[0] = "0";
-        mesa[2] = "0";
-        filosofosComem(fome,mesa);
+    char *aux = verificadorFome(fome,mesa);
+    mesa = resetGarfos(mesa);
+    for(int i = 0; i < 2; i++){
+        if (aux == "fome1" && fome[0]>0){
+            printf("Filoso 1 comendo.\n");
+            fome[0] -= 50;
+            mesa[0] = "0";
+            mesa[2] = "0";
+            aux = verificadorFome(fome,mesa);
+        }else if(aux == "fome2" && fome[1]>0){
+            printf("Filoso 2 comendo.\n");
+            fome[1] -= 50;
+            mesa[2] = "0";
+            mesa[4] = "0";
+            aux = verificadorFome(fome,mesa);
+        }else if(aux == "fome3" && fome[2]>0){
+            printf("Filoso 3 comendo.\n");
+            fome[2] -= 50;
+            mesa[4] = "0";
+            mesa[6] = "0";
+            aux = verificadorFome(fome,mesa);
+        }else if(aux == "fome4" && fome[3]>0){
+            printf("Filoso 4 comendo.\n");
+            fome[3] -= 50;
+            mesa[6] = "0";
+            mesa[8] = "0";
+            aux = verificadorFome(fome,mesa);
+        }else if(aux == "fome5" && fome[4]>0){
+            printf("Filoso 5 comendo.\n");
+            fome[4] -= 50;
+            mesa[8] = "0";
+            mesa[0] = "0";
+            aux = verificadorFome(fome,mesa);
+        }
     }
+    
+    printf("verificarFome = %s\n",aux);
+    imprimir(mesa,fome);
+    printf("\n");
+    
+    printf("Fome[0] = %d\n",fome[0]);
+    printf("Fome[1] = %d\n",fome[1]);
+    printf("Fome[2] = %d\n",fome[2]);
+    printf("Fome[3] = %d\n",fome[3]);
+    printf("Fome[4] = %d\n",fome[4]);
+    
+    sleep(1);
+    filosofosComem(fome,mesa);
+
 }
 
 int main(){
@@ -106,7 +168,7 @@ int main(){
     mesa[9] = "F5";
     
     filosofosComem(fome,mesa);
-
+    
     // imprimir lista
 
     return 0;
